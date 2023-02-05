dc817acbf20490f918ac34443c86f73749fca44f
Abdul Rehman Rais <abdulrehmanrais123@gmail.com>
2023-02-05 11:16:16 +0500

    Update settings.py

--- dms/settings.py
+++ dms/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'dms.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
