904022535da5ff5c49142857cf036c980d069a69
Abdul Rehman Rais <abdulrehmanrais123@gmail.com>
2023-02-05 11:37:58 +0500

    Update settings.py

--- dms/settings.py
+++ dms/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
-#STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATICFILES_DIRS = [
+  #  BASE_DIR / "static",
+ #   '/var/www/static/',
+#]
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
