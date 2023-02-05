222050362417d065577c27ef6d0a096006943387
Abdul Rehman Rais <abdulrehmanrais123@gmail.com>
2023-02-05 11:35:11 +0500

    Update settings.py

--- dms/settings.py
+++ dms/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    '/var/www/static/',
 ]
 #STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
