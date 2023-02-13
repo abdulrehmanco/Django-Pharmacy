052dd1b51b33f68df48394aa5b1760c57c8f6193
Abdul Rehman Rais <abdulrehmanrais123@gmail.com>
2023-02-13 12:20:35 +0500

    Update settings.py

--- dms/settings.py
+++ dms/settings.py
@@ -66,11 +66,15 @@ WSGI_APPLICATION = 'dms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'postgres',
+      'USER':'postgres',
+      'PASSWORD':'admin123',
+      'HOST':'https://django-pharmacy.vercel.app/',
+      'PORT':'5432',
+   }
 }
 
 
